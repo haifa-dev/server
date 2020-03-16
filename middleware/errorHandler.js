d37569f0f25ca9322e19f790fcb2e13601684413
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2020-03-16 22:55:36 +0200

    cleaned test.env, fixed errorhandler functio

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -42,7 +42,7 @@ const sendErrorDev = (err, res) => {
 /**
  * Handle express errors base on origin, environment and status code.
  */
-module.exports = async (err, req, res) => {
+module.exports = async (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'ERROR';
 
